8e75a7a8997140c237f526e7e80a1ed65bca6fa5
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-14 15:49:55 +0100

    feat: evento emit su bottone

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
-<script></script>
+<script>
+import AppSearch from "./components/AppSearch.vue";
+
+export default {
+  components: {
+    AppSearch,
+  },
+  methods: {
+    searchFilm() {
+      console.log("ciao");
+    },
+  },
+};
+</script>
 
 <template>
   <h1>Boolflix</h1>
+  <AppSearch @buttonSearch="searchFilm" />
 </template>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from "../store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <label for="search">Cerca qui i tuoi film </label>
+  <input v-model="store.filmSearched" type="text" name="search" />
+  <button @click="$emit('buttonSearch')">invio</button>
+</template>
