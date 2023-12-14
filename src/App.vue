348c37cc384cb10c0d04a30a8563bae178450ce5
CaldatoLuca <lucacaldato15@gmail.com>
2023-12-14 18:01:15 +0100

    feat: componente visualizzazione ricerca

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script>
 import AppSearch from "./components/AppSearch.vue";
+import AppResoult from "./components/AppResoult.vue";
+import axios from "axios";
+import { store } from "./store";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
   components: {
     AppSearch,
+    AppResoult,
   },
   methods: {
     searchFilm() {
-      console.log("ciao");
+      this.store.apiUrl =
+        "https://api.themoviedb.org/3/search/movie?api_key=21158f75b2a3edbfcfc831b6fd1520bf&query=";
+      this.store.apiUrl += this.store.filmSearched;
+      axios.get(this.store.apiUrl).then((apiData) => {
+        this.store.films = apiData.data.results;
+      });
+      this.store.filmSearched = "";
     },
   },
 };
@@ -16,4 +32,7 @@ export default {
 <template>
   <h1>Boolflix</h1>
   <AppSearch @buttonSearch="searchFilm" />
+  <AppResoult v-if="store.films.length !== 0" />
+
+  <div v-else>Nessun elemnto trovato</div>
 </template>
---
+++ src/components/AppResoult.vue
@@ -0,0 +1,57 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <h2>Titolo</h2>
+      <ul>
+        <li v-for="title in store.films">
+          {{ title.title }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2>Titolo originale</h2>
+      <ul>
+        <li v-for="originalTitle in store.films">
+          {{ originalTitle.original_title }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2>Lingua</h2>
+      <ul>
+        <li v-for="language in store.films">
+          {{ language.original_language }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <h2>Voto</h2>
+      <ul>
+        <li v-for="vote in store.films">
+          {{ vote.vote_average }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+}
+h2 {
+  text-align: center;
+}
+</style>
